97790f9548e66ecf41ba23ec4eb20ae30888bd96
Ara Gamaliel Boanerges Simarmata <ara.boanerges@shopee.com>
2025-03-07 11:02:23 +0700

    add test case function

--- exercises/02.test-structure/02.problem.test-blocks/greet.js
+++ exercises/02.test-structure/02.problem.test-blocks/greet.js
@@ -1,61 +1,51 @@
-function greet(name: string) {
-  return `Hello, ${name}!`;
+console.log("HAHHAHAH");
+function greet(name) {
+    return "Hello, ".concat(name, "!");
 }
-
-function congratulate(name: string) {
-  return `Congrats, ${name}!`;
+function congratulate(name) {
+    return "Congrats, ".concat(name, "!");
 }
-
-function test(testName: string, testBlock: () => void) {
-  try {
-    testBlock();
-    console.log(`✅ ${testName}\n`);
-  } catch (err) {
-    console.log(`❌ ${testName}\n${err}\n`);
-  }
+function test(testName, testBlock) {
+    try {
+        testBlock();
+        console.log("\u2705 ".concat(testName));
+    }
+    catch (err) {
+        console.log("\u274C ".concat(testName, " ").concat(err.message));
+    }
 }
-
 // 🐨 Wrap each of the existing "expect()" calls in the
 // "test()" function you will write later down this file.
-
 // 💰 Use meaningful test case names, like:
 // - returns a greeting message for the given name
 // - returns a congratulation message for the given name
-
 // 💰 Provide the callback to the "test()" function that
 // will run these assertions.
 // test('', () => {
 //  expect(greet('John')).toBe('Hello, John!')
 // })
-
-
-function expect(actual: unknown) {
-  return {
-    toBe(expected: unknown) {
-      if (actual !== expected) {
-        throw new Error(`Expected ${actual} to equal to ${expected}`);
-      }
-    },
-  };
+function expect(actual) {
+    return {
+        toBe: function (expected) {
+            if (actual !== expected) {
+                throw new Error("Expected ".concat(actual, " to equal to ").concat(expected));
+            }
+        },
+    };
 }
-
-test("Should return the correct greet Taylor Swift", () => {
-  expect(greet('John')).toBe('Hello, Taylor Swift!');
+test("Should return the correct greet Taylor Swift", function () {
+    expect(greet('John')).toBe('Hello, Taylor Swift!');
 });
-
-test("Should return the correct congrats Taylor Swift", () => {
-  expect(congratulate('Sarah')).toBe('Congrats, Sarah!');
+test("Should return the correct congrats Taylor Swift", function () {
+    expect(congratulate('Sarah')).toBe('Congrats, Sarah!');
 });
-
 // 🐨 Create a new function called "test".
 // The "test" function accepts two arguments: "title" and "callback".
-
 // 🐨 In the "test" function, call the "callback" function
 // and print a message to the console if it completes without throwing.
 // Include a checkbox icon and the test's title in the message.
 // 💰 See if you can *try* and *catch* any errors from the "callback".
 // 💰 Use this template for success messages: `✓ ${title}`
-
 // 🐨 If the calling the "callback" function throws an error,
 // print another message to the console. In this one, include a cross
 // icon, the test's name, and the thrown error.
