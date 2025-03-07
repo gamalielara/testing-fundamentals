ed9d6c73c6d22a1b6ff783d0ee3fc4101162308a
Ara Gamaliel Boanerges Simarmata <ara.boanerges@shopee.com>
2025-03-07 10:46:21 +0700

    exercise assertion

--- exercises/02.test-structure/01.problem.assertions/greet.js
+++ exercises/02.test-structure/01.problem.assertions/greet.js
@@ -1,34 +1,27 @@
-function greet(name: string) {
-	return `Hello, ${name}!`
+function greet(name) {
+    return "Hello, ".concat(name, "!");
 }
-
-function congratulate(name: string) {
-	return `Congrats, ${name}!`
+function congratulate(name) {
+    return "Congrats, ".concat(name, "!");
 }
-
-function expect(actual: string){
-	return {
-		toBe: (expected: string) => {
-			if(expected !== actual){
-				throw new Error(`Expected message to equal to "${expected}" but got "${actual}"`)
-			}
-		}
-	}
+function expect(actual) {
+    return {
+        toBe: function (expected) {
+            if (expected !== actual) {
+                throw new Error("Expected message to equal to \"".concat(expected, "\" but got \"").concat(actual, "\""));
+            }
+        }
+    };
 }
-
-expect(greet("Taylor Swift")).toBe("Hello, Taylor Swift")
-expect(congratulate("Taylor Swift")).toBe("Congrats, Taylor Swift")
-
-
+expect(greet("Taylor Swift")).toBe("Hello, Taylor Swift");
+expect(congratulate("Taylor Swift")).toBe("Congrats, Taylor Swift");
 // 🐨 Create a function called "expect" that accepts a single argument:
 // the "actual" value.
-
 // 🐨 From the "expect" function, return an object that has
 // a "toBe" property, which equals to a new function.
 // The "toBe" function accepts the "expected" argument.
 // 💰 Use shorthand function declaration on objects:
 // return { toBe(expected) {} }
-
 // 🐨 In the "toBe" function, compare the "actual" and the "expected"
 // values, and throw an error if they don't mtach.
 // 💰 You can throw an error like this one:
